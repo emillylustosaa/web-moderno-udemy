80dfde3bc011feddaceb5de7e38e7d62c9632295
emillylustosaa <lustosaemilly@gmail.com>
2020-08-03 16:24:54 -0300

    Ajuste na pasta fundamentos,estudo sobre funções e estruturas de controle

--- exercicios-js/fundamentos/numerosAlgunscuidados.js
+++
@@ -1,8 +0,0 @@
-console.log(7 / 0) // Infinity
-console.log("10" / 2) // = 5
-console.log('3' + 2) // String ganha!
-console.log('3' - 2)
-console.log("Show!" * 2) // NaN
-console.log(0.1 + 0.7) // = 0.799999999999
-// console.log(10.toString())
-console.log((10.345).toFixed(2)) // = 10.35
